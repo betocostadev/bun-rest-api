37d88d2d6221ec963f8260cf5fadbfe36766db15
betocostadev <robertomarx@gmail.com>
2024-03-01 19:45:05 -0300

    Added some small data for testing

---
+++ src/fake_data/posts.ts
@@ -0,0 +1,21 @@
+import { post } from '../@types/posts'
+
+const posts: post[] = [
+  {
+    id: '1',
+    title: 'First Post',
+    content: 'This is the first post',
+  },
+  {
+    id: '2',
+    title: 'Second Post',
+    content: 'This is the second post',
+  },
+  {
+    id: '3',
+    title: 'Third Post',
+    content: 'This is the third post',
+  },
+]
+
+export default posts
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia'
+import posts from './fake_data/posts'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get('/', () => 'Hello Elysia').listen(3000)
 
+app.get('/posts/:id', ({ params }) => {
+  const post = posts.find((post) => post.id === params.id)
+  if (!post) return 'Post not found'
+  return `Post ID: ${post.id}, Title: ${post.title}, Content: ${post.content}`
+})
+
+app.get('/posts', () => {
+  console.log(posts)
+  return JSON.stringify(posts)
+})
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+)
